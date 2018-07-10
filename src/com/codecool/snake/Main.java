9b1268dc13b47b26853f607cbd79274c78c04f9d
Pawel Krupa <pawel.krupa.junior@gmail.com>
2018-07-10 15:09:34 +0200

    Temporary main menu test.

--- src/com/codecool/snake/Main.java
+++ src/com/codecool/snake/Main.java
@@ -1,6 +1,8 @@
 package com.codecool.snake;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -11,11 +13,12 @@ public class Main extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws Exception {
         Game game = new Game();
+        Parent root = FXMLLoader.load(getClass().getResource(MainMenuController.MenuFXML));
 
         primaryStage.setTitle("Snake Game");
-        primaryStage.setScene(new Scene(game, Globals.WINDOW_WIDTH, Globals.WINDOW_HEIGHT));
+        primaryStage.setScene(new Scene(root, Globals.WINDOW_WIDTH, Globals.WINDOW_HEIGHT));
         primaryStage.show();
         game.start();
     }
