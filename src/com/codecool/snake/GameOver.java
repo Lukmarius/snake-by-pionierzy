f6d625f5a4d5f3fa161a526be697566488564e6d
Mariusz J <mariusz.jaskola.92@gmail.com>
2018-07-10 15:35:32 +0200

    modal's size changed

--- src/com/codecool/snake/GameOver.java
+++ src/com/codecool/snake/GameOver.java
@@ -6,7 +6,11 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -15,14 +19,19 @@ public class GameOver {
     public void showPopUp() {
         Stage modalStage = new Stage();
         modalStage.initModality(Modality.APPLICATION_MODAL);
-        modalStage.initStyle(StageStyle.UNDECORATED);
+        modalStage.initStyle(StageStyle.DECORATED);
 
         Button replayBtn = new Button("Play again");
         //replayBtn.setOnAction(event -> Main.restart()); -------RESTART
         Button exitBtn = new Button("Exit game");
         exitBtn.setOnAction(event -> Platform.exit());
 
-        VBox vbox = new VBox(new Text("You won!"), replayBtn, exitBtn);
+        Text t = new Text("GAME OVER");
+        t.setTextAlignment(TextAlignment.CENTER);
+        t.setFont(Font.font("Arial",80));
+
+        //t.setSelectionFill(Paint.valueOf("red"));
+        VBox vbox = new VBox(t, replayBtn, exitBtn);
         vbox.setAlignment(Pos.CENTER);
         vbox.setPadding(new Insets(25));
         vbox.setSpacing(15);
