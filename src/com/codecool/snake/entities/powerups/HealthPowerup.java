ad936603bce5a0fe6fd62c1e6ce9805f10534e28
Pico143 <pico19942@gmail.com>
2018-07-11 11:02:27 +0200

    added health powerup

--- src/com/codecool/snake/entities/powerups/HealthPowerup.java
+++ src/com/codecool/snake/entities/powerups/HealthPowerup.java
@@ -1,23 +1,33 @@
 package com.codecool.snake.entities.powerups;
 
+import com.codecool.snake.Globals;
 import com.codecool.snake.entities.GameEntity;
 import com.codecool.snake.entities.Interactable;
 import com.codecool.snake.entities.snakes.SnakeHead;
 import javafx.scene.layout.Pane;
 
+import java.util.Random;
+
 public class HealthPowerup extends GameEntity implements Interactable {
 
-    protected HealthPowerup(Pane pane) {
+    public HealthPowerup(Pane pane) {
         super(pane);
+        setImage(Globals.powerupBerry);
+        pane.getChildren().add(this);
+
+        Random rnd = new Random();
+        setX(rnd.nextDouble() * Globals.GAME_WIDTH);
+        setY(rnd.nextDouble() * Globals.GAME_HEIGHT);
     }
 
     @Override
     public void apply(SnakeHead snakeHead) {
-
+        snakeHead.healthProperty.add(10);
+        destroy();
     }
 
     @Override
     public String getMessage() {
-        return null;
+        return "Got +10 health";
     }
 }
