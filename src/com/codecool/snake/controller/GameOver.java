2b38c7f5a42b7701a44cdbaff4b9d07399ed5401
Pawel Krupa <pawel.krupa.junior@gmail.com>
2018-07-13 01:05:57 +0200

    Update GameOver.java -> stylistic improvement

--- src/com/codecool/snake/controller/GameOver.java
+++ src/com/codecool/snake/controller/GameOver.java
@@ -1,16 +1,13 @@
 package com.codecool.snake.controller;
 
 import com.codecool.snake.Globals;
-import com.codecool.snake.controller.MainController;
-import com.codecool.snake.entities.GameEntity;
-import com.codecool.snake.entities.snakes.SnakeHead;
 import javafx.application.Platform;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
@@ -41,16 +38,24 @@ public class GameOver {
         Button exitBtn = new Button("Exit game");
         exitBtn.setOnAction(event -> Platform.exit());
 
+        exitBtn.getStyleClass().add("backBtn");
+        replayBtn.getStyleClass().add("menuBtn");
+
+        HBox hBox = new HBox(exitBtn, replayBtn);
+        hBox.setAlignment(Pos.CENTER);
+        hBox.setSpacing(20);
+
         Text t = new Text("GAME OVER");
         t.setTextAlignment(TextAlignment.CENTER);
         t.setFont(Font.font("Arial",80));
 
         t.setFill(Paint.valueOf("Red"));
         t.setStroke(Paint.valueOf("Black"));
-        VBox vbox = new VBox(t, replayBtn, exitBtn);
+        VBox vbox = new VBox(t, hBox);
         vbox.setAlignment(Pos.CENTER);
         vbox.setPadding(new Insets(25));
         vbox.setSpacing(15);
+        vbox.getStylesheets().add(getClass().getResource("/css/style.css").toExternalForm());
 
         modalStage.setScene(new Scene(vbox));
         modalStage.show();
